9eb4d385c090e5135ef077674a7edde3f8d135d0
mfarnicki <mfarnicki@hotmail.com>
2022-12-10 19:12:28 +0100

    Initial commit

---
+++ Data/EmployeesDbContext.cs
@@ -0,0 +1,14 @@
+using Employees.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.API.Data
+{
+    public class EmployeesDbContext : DbContext
+    {
+        public EmployeesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees {get;set;}
+    }
+}
