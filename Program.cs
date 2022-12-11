ac392d1e45014545e8fae389e9a8e8385e62bc5c
mfarnicki <mfarnicki@hotmail.com>
2022-12-11 01:40:09 +0100

    Allow any origin

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(builder => 
-    builder.WithOrigins(app.Configuration.GetValue<string>("AllowedOrigin")).AllowAnyHeader().AllowAnyMethod()
+    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
 );
 
 app.UseAuthorization();
